15864e61a42465263068950837fd2b7f92483a33
fuad sheri <fuadpuadun@gmail.com>
2022-10-22 20:21:58 +0700

    Update Ref

--- src/tk3/No1.java
+++ src/tk3/No1.java
@@ -5,7 +5,10 @@ import java.util.Scanner;
 public class No1 {
 	  public static void main(String[] args) {
 		  boolean next = true;
-		  int score=0;
+		  int score1=0;
+		  int score2=0;
+		  int score3=0;
+		  int ovrScore=0;
 		  
 		  Scanner input = new java.util.Scanner(System.in);
 		  
@@ -25,35 +28,49 @@ public class No1 {
 			  //get enter
 			  input.nextLine();
 			  
-			  score = level1(score);
+			  score1=0;
+			  score1 = level1(score1);
 			  //Test
 			  //System.out.print (score);
 			  
-			  if(score > 70) {
-				  score = 0;
+			  // level
+			  if(score1 >= 70) {
+				  score2 = 0;
+				  score2 = level2(score2);
 			  }
 			  
-			  //develop to level 2
-			  //score = level2(score);
-			  
-			  if(score > 70) {
-				  score = 0;
+			  //level 3
+			  if(score2 >= 70) {
+				  score3 = 0;
+				  score3 = level3(score3);
 			  }
 			  
-			  //develop to level 3
-			  //score = level3(score);
 			  
-		      System.out.print ("\n Anda mau ulang [y/t] : ");
-		      String check = input.nextLine();
-		      
-		      if (check.equalsIgnoreCase ("Y"))
-					next = true;
-				else if (check.equalsIgnoreCase ("T"))
-					next = false;
-				else {
-					System.out.print ("Input yang diberikan salah !\n");
-					continue label;
-				}
+			  if((score1 < 70) || (score2 < 70) || (score3 < 70)) {
+				  System.out.println("You Lose!! Try Again..");
+				  
+				  System.out.print ("\n Anda mau ulang [y/t] : ");
+			      String check = input.nextLine();
+			      
+			      if (check.equalsIgnoreCase ("Y"))
+						next = true;
+					else if (check.equalsIgnoreCase ("T"))
+						next = false;
+					else {
+						System.out.print ("Input yang diberikan salah !\n");
+						continue label;
+					}
+			  } else {
+				  ovrScore=0;
+				  ovrScore = score1+score2+score3;
+				  System.out.print ("Overall score : " + ovrScore + "\n");
+				  System.out.print ("You Win !\n");
+				  System.out.print ("Press Enter to Exit!");
+				  //get enter
+				  input.nextLine();
+				  
+				  next = false;
+			  }		      
 		  }
 	  }
 	  
@@ -64,7 +81,7 @@ public class No1 {
                   "tilted","titled"};
 		  String[] checker = new String[10];
 		  int x=0;
-		  boolean test;
+		  int answer;
 		  
 		  System.out.println(""
 	                +"Level 1"+"\n"
@@ -96,8 +113,106 @@ public class No1 {
 						  }
 					  }
 				  }
-			  } else System.out.println("Answer too short");
+			  } else System.out.println("Answer either too short or too long");
 		  }
+		  answer = score / 10;
+		  System.out.println("You had answer 10 times with " + answer + " right answers..");
+		  
 		  return score;
+		  
+	  }
+	  
+	  public static int level2(int score) {
+		  //init KJ
+		  String[] level1={"", "ace", "can", "sac", "sea", "sec", "see", "aces", "acne", "cane", "cans", "\n",
+		            "case", "ease", "sane", "scan","seen","acnes","canes","cease","cense","scene", "\n",
+		            "encase", "seance"};
+		  String[] checker = new String[10];
+		  int x=0;
+		  int answer;
+		  
+		  System.out.println("\n\n"
+	                +"Level 2"+"\n"
+	                +"-------"+"\n"
+	                +"s     e     c     a     e     n"+"\n");
+		  
+		  Scanner scan = new java.util.Scanner(System.in);
+		  
+		  String guess="";
+		  
+		  for(int i= 1; i<=10;i++) {
+			  System.out.print(i+"> Your Answer: ");
+			  guess=scan.nextLine();
+			  //check strLength
+			  if(guess.length()>=3 && guess.length()<=6) {
+				  for(int j= 0; j<= level1.length - 1;j++) {
+					  if(guess.equals(level1[j])) {						  
+						  for(int k= 0; k<= checker.length - 1;k++) {
+							  if(guess.equals(checker[k])) {
+								  System.out.println("You had already type this word before..");
+								  k=checker.length;
+							  } else if(k==checker.length-1) {
+								  score+=10;
+								  System.out.println("#Right. Score : " + score);
+								  checker[x] = guess;
+								  k=checker.length;
+								  x++;
+							  }
+						  }
+					  }
+				  }
+			  } else System.out.println("Answer either too short or too long");
+		  }
+		  answer = score / 10;
+		  System.out.println("You had answer 10 times with " + answer + " right answers..");
+		  
+		  return score; 
+	  }
+	  
+	  public static int level3(int score) {
+		  //init KJ
+		  String[] level1={"", "eon", "hen", "her", "hoe", "hon", "ken", "nor", "one", "ore", "rho", "\n",
+                  "roe", "hero", "hoer", "hone", "honk", "horn", "kern", "oner", "heron", "honer", "\n",
+                  "honker"};
+		  String[] checker = new String[10];
+		  int x=0;
+		  int answer;
+		  
+		  System.out.println("\n\n"
+	                +"Level 3"+"\n"
+	                +"-------"+"\n"
+	                +"h     k     r     n     e     o"+"\n");
+		  
+		  Scanner scan = new java.util.Scanner(System.in);
+		  
+		  String guess="";
+		  
+		  for(int i= 1; i<=10;i++) {
+			  System.out.print(i+"> Your Answer: ");
+			  guess=scan.nextLine();
+			  //check strLength
+			  if(guess.length()>=3 && guess.length()<=6) {
+				  for(int j= 0; j<= level1.length - 1;j++) {
+					  if(guess.equals(level1[j])) {						  
+						  for(int k= 0; k<= checker.length - 1;k++) {
+							  if(guess.equals(checker[k])) {
+								  System.out.println("You had already type this word before..");
+								  k=checker.length;
+							  } else if(k==checker.length-1) {
+								  score+=10;
+								  System.out.println("#Right. Score : " + score);
+								  checker[x] = guess;
+								  k=checker.length;
+								  x++;
+							  }
+						  }
+					  }
+				  }
+			  } else System.out.println("Answer either too short or too long");
+		  }
+		  answer = score / 10;
+		  System.out.println("You had answer 10 times with " + answer + " right answers..");
+		  
+		  return score; 
 	  }
 }
